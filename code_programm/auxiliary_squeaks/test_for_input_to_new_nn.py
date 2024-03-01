944804d39162f4575307f7937cf033d42c486344
Filisss_911 <102216221+FilippYV@users.noreply.github.com>
2024-03-01 04:34:45 +0300

    Начало работы над логикой новой нейронной сети

--- code_programm/auxiliary_squeaks/test_for_input_to_new_nn.py
+++ code_programm/auxiliary_squeaks/test_for_input_to_new_nn.py
@@ -1,6 +1,7 @@
 import cv2
 import torch
 from ultralytics import YOLO
+import numpy as np
 
 from code_programm.path import get_path_weight_model
 
@@ -10,14 +11,48 @@ print(torch.cuda.get_device_name())
 model = YOLO(get_path_weight_model('best_v2_n.pt'))
 
 img = cv2.imread(r'C:\PycharmProjects\ETS_Autopilot\static\settings_cache\1708818112.041754.png')
-print(img.shape)
+results = model(img)
 
-results = model(img, imgsz=512, conf=0.75, verbose=True, show=True, device='cpu')
 
-H, W, _ = img.shape
+# Получение классов и имен классов
+classes = results[0].boxes.cls.cpu().numpy()
+class_names = results[0].names
 
-for result in results:
-    for j, mask in enumerate(result.masks.data):
-        mask = mask.numpy() * 255
-        mask = cv2.resize(mask, (W, H))
-        cv2.imwrite('output.png', mask)
+# Получение бинарных масок и их количество
+masks = results[0].masks.data  # Формат: [число масок, высота, ширина]
+num_masks = masks.shape[0]
+
+
+# Создание изображения для отображения масок
+mask_overlay = np.zeros_like(img)
+
+labeled_image = img.copy()
+
+# Добавление подписей к маскам
+print(num_masks)
+for i in range(num_masks):
+    mask = masks[i].cpu()
+
+    # Изменение размера маски до размеров исходного изображения с использованием метода ближайших соседей
+    mask_resized = cv2.resize(np.array(mask), (img.shape[1], img.shape[0]), interpolation=cv2.INTER_NEAREST)
+
+    print(mask_resized)
+
+    # Получение класса для текущей маски
+    class_index = int(classes[i])
+    class_name = class_names[class_index]
+
+    # Добавление подписи к маске
+    mask_contours, _ = cv2.findContours(mask_resized.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(labeled_image, mask_contours, -1, color, 1)
+    cv2.putText(labeled_image, class_name,
+                (int(mask_contours[0][:, 0, 0].mean()), int(mask_contours[0][:, 0, 1].mean())),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, color, 1)
+
+# Отобразите итоговое изображение с наложенными масками и подписями
+plt.figure(figsize=(8, 8), dpi=150)
+labeled_image = cv2.cvtColor(labeled_image, cv2.COLOR_BGR2RGB)
+plt.imshow(labeled_image)
+plt.axis('off')
+plt.savefig('output.png')
+plt.show()
